937e67eec6bbf6f43866925d2e1e253ccfec688e
Ralf Hergert <ralf.hergert@gmx.de>
2016-06-26 10:50:08 +0200

    changed wording of comments a little

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ jQuery([selector]).normalize();
 
 DOM Normalization is the process of joining together adjacent textNodes. Regarding to the DOM specification (Core Level 2 Node Object) there is already a method named "normalize" specified, but IE apparently has a bug in joining two text nodes if they are split by a soft or regular hyphen.
 
-This is where this extension may help: It figures whether the native normalize implementation works and then eighter referes to the native implementation or it uses a fallback implementation for normalization.
+This is where this extension may help: It determines whether the native implementation for normalize works and then eighter referes to it or a fallback implementation.
--- jquery.normalize.js
+++ jquery.normalize.js
@@ -1,8 +1,9 @@
 /**@preserve https://github.com/ralfhergert/jquery-normalize
  * jQuery wrapper for DOM normalization */
 /** Normalization is the process of joining together adjacent textNodes. In DOM
- * there is node-method called normalize specified for it, but IE apparently has
- * a bug joining two text node if they are split by a soft or regular hyphen.
+ * there is node-method called normalize specified for this task, but apparently
+ * IE has a bug joining two text nodes if they are split by a soft or regular
+ * hyphen.
  * This jQuery extension will test whether the native browser implementation
  * works, else it will use a fallback implementation. */
 (function(jQuery) {
@@ -12,9 +13,11 @@
 		wrapper.appendChild(document.createTextNode('a'));
 		wrapper.appendChild(document.createTextNode('\u00ad')); // append a soft hyphen
 		wrapper.appendChild(document.createTextNode('b'));
-		/* in IE normalize will only misbehave, when the wrapper is actually added to the DOM, else normalize works correctly.
-		 * since the document.body does not exist right now, the document.head is used. */
-		var dom = document.body || document.head;
+		/* in IE normalize will only misbehave, when the wrapper is actually added to the DOM,
+		 * else normalize might work correctly. Also IE might switch modes after the document
+		 * head is parsed, which means doing this check too early will not discover a faulty
+		 * normalize. */
+		var dom = document.body || document.head; // fallback to head to prevent an exception.
 		dom.appendChild(wrapper);
 		wrapper.normalize();
 		var numberOfChildren = wrapper.childNodes.length;
